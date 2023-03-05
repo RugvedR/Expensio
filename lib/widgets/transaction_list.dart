2b92e470905c0a88085d712102e6d8a52433039f
RugvedR <rugvedr9@gmail.com>
2023-03-05 14:35:33 +0530

    listtile added in transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -14,52 +14,81 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: transactions.isEmpty ? Column(
-        children: [
-          Text('No transactions added yet!', style: Theme.of(context).textTheme.headline6,),
-          SizedBox(height: 20,),
-          Container(
-            height: 200,
-            child: Image.asset('assets/images/waiting.png',fit: BoxFit.cover,)
-          )
-        ],
-      ) : ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            child: Row(
+      child: transactions.isEmpty
+          ? Column(
               children: [
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Theme.of(context).primaryColor, width: 1)),
-                  padding: EdgeInsets.all(8),
-                  child: Text(
-                    '\u{20B9} ${transactions[index].amount.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Theme.of(context).primaryColor),
-                  ),
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6
-                    ),
-                    Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                )
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: FittedBox(
+                          child: Text(
+                            '\u{20B9} ${transactions[index].amount.toStringAsFixed(2)}',
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(transactions[index].title, style: Theme.of(context).textTheme.title,),
+                    subtitle: Text(DateFormat.yMMMd().format( transactions[index].date)),
+                  ),
+                );
+              },
+              itemCount: transactions.length,
             ),
-          );
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
+
+
+// Card(
+//             child: Row(
+//               children: [
+//                 Container(
+//                   margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+//                   decoration: BoxDecoration(
+//                       border: Border.all(color: Theme.of(context).primaryColor, width: 1)),
+//                   padding: EdgeInsets.all(8),
+//                   child: Text(
+//                     '\u{20B9} ${transactions[index].amount.toStringAsFixed(2)}',
+//                     style: TextStyle(
+//                         fontWeight: FontWeight.bold,
+//                         fontSize: 18,
+//                         color: Theme.of(context).primaryColor),
+//                   ),
+//                 ),
+//                 Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text(
+//                       transactions[index].title,
+//                       style: Theme.of(context).textTheme.headline6
+//                     ),
+//                     Text(
+//                       DateFormat.yMMMd().format(transactions[index].date),
+//                       style: TextStyle(color: Colors.grey),
+//                     ),
+//                   ],
+//                 )
+//               ],
+//             ),
+//           );
