3eddfdad5569022d466713238dfff38b11dfac7e
RugvedR <rugvedr9@gmail.com>
2023-03-05 14:45:26 +0530

    charts updated a littl

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,7 +13,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(child: Text('\u{20B9} ${spendingAmount.toStringAsFixed(0)} ')),
+        Container(
+          height: 20,
+          child: FittedBox(child: Text('\u{20B9} ${spendingAmount.toStringAsFixed(0)} '))),
         SizedBox(height: 4,),
         Container(
           height: 60,
