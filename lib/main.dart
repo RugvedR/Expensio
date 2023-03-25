86aeaefc3c1b5dbc69e3712c4edf9153521b0854
RugvedR <rugvedr9@gmail.com>
2023-03-25 14:56:51 +0530

    new layout switch for landscape orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/services.dart';
+
 import './widgets/chart.dart';
 
 import './widgets/transaction_list.dart';
@@ -10,6 +12,11 @@ import './widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown,
+  // ]);
   runApp(MyApp());
 }
 
@@ -65,6 +72,8 @@ class _MyHomePageState extends State<MyHomePage> {
     Transaction(id: 't2', title: 'Something', amount: 78.29, date: DateTime.now()),
   ];
 
+  bool _showChart = false;
+
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
         
@@ -99,27 +108,45 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
     final appBar = AppBar(
         title: Text('Expensio'),
         actions: [
           IconButton(onPressed: ()=> _startAddNewTransaction(context), icon: Icon(Icons.add)),
         ],
       );
+    final txListWidget = Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
+              child: TransactionList(_userTransactions, _deleteTransaction)
+            );
     return Scaffold(
       appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.3,
+            if(isLandscape) Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Show Chart'),
+                Switch(value: _showChart, onChanged: (val){
+                  setState(() {
+                    _showChart = val;
+                  });
+                },),
+              ],
+            ),
+            if(!isLandscape) Container(
+              height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.4,
               child: Chart(_recentTransactions)
             ),
-            
-            Container(
+            if(!isLandscape)  txListWidget,
+            if(isLandscape) _showChart ? Container(
               height: (MediaQuery.of(context).size.height - appBar.preferredSize.height - MediaQuery.of(context).padding.top) * 0.7,
-              child: TransactionList(_userTransactions, _deleteTransaction)
-            ),
+              child: Chart(_recentTransactions)
+            ) 
+            : txListWidget//--> when _showChart is false show the below container
+            
           ],
         ),
       ),
